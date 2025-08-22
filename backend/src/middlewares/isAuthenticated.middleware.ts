6e8cb171b9653557301710143356146b2a0aba6d
Satyam Laheri <satyamlaheri3@gmail.com>
2025-08-23 01:56:26 +0530

    Update isAuthenticated.middleware.ts

--- backend/src/middlewares/isAuthenticated.middleware.ts
+++ backend/src/middlewares/isAuthenticated.middleware.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { UnauthorizedException } from "../utils/appError";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    console.log("isAuthenticated middleware - req.user:", req.user);
+    console.log("isAuthenticated middleware - req.session:", req.session);
+    console.log("isAuthenticated middleware - cookies:", req.headers.cookie);
 
     if(!req.user || !req.user._id) {
         throw new UnauthorizedException('Unauthorized. Please login to continue');
